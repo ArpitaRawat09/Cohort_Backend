069e5a9cb3ed25d029eec98753c65296b98bcf45
Arpita Rawat <arpitarawat381@gmail.com>
2025-07-30 01:02:46 +0530

    learn token,jwt and cookies

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const authRoutes = require('./routes/auth.routes')
+const authRoutes = require("./routes/auth.routes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
-app.use('/',authRoutes)
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/", authRoutes);
 
 module.exports = app;
