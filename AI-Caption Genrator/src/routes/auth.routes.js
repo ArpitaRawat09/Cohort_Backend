88f01cbaffe980ca15b42659ade99646a41a12b6
Arpita Rawat <arpitarawat381@gmail.com>
2025-08-01 01:20:43 +0530

    implement hashing

--- AI-Caption Genrator/src/routes/auth.routes.js
+++ AI-Caption Genrator/src/routes/auth.routes.js
@@ -1,34 +1,9 @@
 const express = require("express");
-const userModel = require("../models/user.model");
-const jwt = require("jsonwebtoken");
+const { registerController, loginController } = require("../controllers/auth.controller");
 
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-
-  const userExisting = await userModel.findOne({
-    username,
-  });
-
-  if (userExisting) {
-    return res.status(409).json({
-      message: " UserName Already Exists",
-    });
-  }
-
-  const user = await userModel.create({
-    username,
-    password,
-  });
-
-  const { token } = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-  res.cookie("token", token);
-
-  res.status(201).json({
-    message: "User created successfully",
-    user,
-  });
-});
+router.post("/register",registerController);
+router.post("/login",loginController)
 
 module.exports = router;
