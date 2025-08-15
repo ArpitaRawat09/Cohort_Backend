08d0eb1bc39b974fc506005304d6a52641ca21a1
Arpita Rawat <arpitarawat381@gmail.com>
2025-08-15 10:29:22 +0530

    add image upload functionality and update dependencies

--- AI-Caption Genrator/src/controllers/post.controller.js
+++ AI-Caption Genrator/src/controllers/post.controller.js
@@ -1,5 +1,7 @@
 const postModel = require("../models/post.model");
 const generateCaption = require("../service/ai.service");
+const uploadFile = require("../service/storage.service");
+const { v4: uuidv4 } = require("uuid");
 
 async function createPostController(req, res) {
   const file = req.file;
@@ -8,12 +10,25 @@ async function createPostController(req, res) {
   const base64Image = new Buffer.from(file.buffer).toString("base64");
 
   const caption = await generateCaption(base64Image);
+  const result = await uploadFile(file.buffer, `${uuidv4()}`);
 
-  // console.log("Generated caption:", caption);
-  res.json({
-    caption,
+  const post = await postModel.create({
+    caption: caption,
+    image: result.url,
+    user: req.userData._id,
+  });
+
+  res.status(201).json({
+    message: "Post created successfully",
+    post,
   });
 }
+
+// console.log("Generated caption:", caption);
+// res.json({
+//   caption,
+//   result,
+// });
 
 module.exports = {
   createPostController,
